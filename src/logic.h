328544628fd6811ee834854ac58723ad39e7a3b0
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-09-19 12:20:00 +0200

    refactor: Rename logic files and encapsulate functions in Logic class

--- src/logic.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef LOGIC_H
-#define LOGIC_H
-#include <bitset>
-
-bool checkForWinningRow(std::bitset<9> & board);
-bool checkForDraw(std::bitset<9> & board1, std::bitset<9> & board2);
-
-#endif //LOGIC_H
